45a12da1f6654cc1e0ef142ff61e08508de11544
kimkyubae <kkyub5118@gmail.com>
2022-02-24 16:21:25 +0900

    overload

---
+++ chapter03/ex04_overload.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 중복 함수
+
+int square(int i)
+{
+    cout << "square(int) 호출" << endl;
+    return i * i;
+}
+
+double square(double i)
+{
+    cout << "square(double) 호출" << endl;
+    return i * i;
+}
+int main(int argc, char const *argv[])
+{
+    cout << square(10) << endl;
+    cout << square(2.0) << endl;
+    return 0;
+}
