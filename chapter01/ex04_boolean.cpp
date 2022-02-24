9f6db59bbc6cb12d4903d7640694f787bfd5316b
kyubae <kkyub5118@gmail.com>
2022-02-24 11:14:03 +0900

    boolalpha

--- chapter01/ex04_boolean.cpp
+++ chapter01/ex04_boolean.cpp
@@ -6,6 +6,10 @@ int main(int argc, char const *arcv[])
     bool b;
     b = true;
 
-    cout << b;
+    cout << b << endl;
+    cout << boolalpha;
+    cout << b << endl;
+    b = false;
+    cout << b << endl;
     return 0;
 } // namespace std;
