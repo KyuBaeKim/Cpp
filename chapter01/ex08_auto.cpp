12083b8e9b3715842e7b07b0988d46019a1f7d5c
kyubae <kkyub5118@gmail.com>
2022-02-24 13:51:44 +0900

    if 수정, output에 rel 파일 저장하게 하기

--- chapter01/ex08_auto.cpp
+++ chapter01/ex08_auto.cpp
@@ -2,16 +2,16 @@
 #include <string>
 using namespace std;
 
-auto add(int x, int y)
-{
-    return x + y;
-}
+// auto add(int x, int y)
+// {
+//     return x + y;
+// }
 
-int main(int argc, char const *argv[])
-{
-    auto d = 1.0; // 값을 보고 double 타입이 됨
-    auto sum = add(5, 10);
+// int main(int argc, char const *argv[])
+// {
+//     auto d = 1.0; // 값을 보고 double 타입이 됨
+//     auto sum = add(5, 10);
 
-    cout << d << " " << sum << endl;
-    return 0;
-}
+//     cout << d << " " << sum << endl;
+//     return 0;
+// }
