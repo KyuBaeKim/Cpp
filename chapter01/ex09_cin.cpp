4d5cd629e525c04c76ce9d3428ebb8bfad7d7ae0
kyubae <kkyub5118@gmail.com>
2022-02-24 13:12:59 +0900

    cin

---
+++ chapter01/ex09_cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    cin >> i;
+    double f;
+    cin >> f;
+
+    cout << i << " " << f << endl;
+    return 0;
+}
